2c5e8a49b1c31a66728e5ec3c4848eb4b72dc61c
GREEN <humains29@gmail.com>
2021-04-20 13:36:57 +0900

    [#1] style: 불필요 주석 제거

--- MyMagicalHand/MyMagicalHand/SceneDelegate.swift
+++ MyMagicalHand/MyMagicalHand/SceneDelegate.swift
@@ -1,9 +1,3 @@
-//
-//  MyMagicalHand - SceneDelegate.swift
-//  Created by yagom. 
-//  Copyright © yagom. All rights reserved.
-// 
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
