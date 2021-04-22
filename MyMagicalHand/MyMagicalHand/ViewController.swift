384c687d322b5b2ad9bc8664dc1bbce1dd7e71a8
Glenn <be9.smile@gmail.com>
2021-04-23 02:50:29 +0900

    [#2] Feat : UI 구조 구현

--- MyMagicalHand/MyMagicalHand/ViewController.swift
+++ MyMagicalHand/MyMagicalHand/ViewController.swift
@@ -7,16 +7,17 @@ class ViewController: UIViewController {
         blurBackgroundView.effect = UIBlurEffect(style: .dark)
         return blurBackgroundView
     }()
+    // TODO: Drawable
     private let drawingView: UIView = {
         let drawingView = UIView()
         drawingView.backgroundColor = .white
         return drawingView
     }()
-    private let labelStackView: UIStackView = {
-        let labelStackView = UIStackView()
-        labelStackView.axis = .horizontal
-        labelStackView.distribution = .equalSpacing
-        return labelStackView
+    private let buttonStackView: UIStackView = {
+        let buttonStackView = UIStackView()
+        buttonStackView.axis = .horizontal
+        buttonStackView.distribution = .equalSpacing
+        return buttonStackView
     }()
     private let showingResultButton: UIButton = {
         let showingResultButton = UIButton(type: .system)
@@ -29,6 +30,30 @@ class ViewController: UIViewController {
         removalButton.setTitle("지우기", for: .normal)
         removalButton.setTitleColor(.systemGray2, for: .normal)
         return removalButton
+    }()
+    private let labelStackView: UIStackView = {
+        let labelStackView = UIStackView()
+        labelStackView.axis = .vertical
+        labelStackView.alignment = .center
+        labelStackView.spacing = 12
+        labelStackView.distribution = .equalSpacing
+        return labelStackView
+    }()
+    private let returnResultLabel: UILabel = {
+        let returnResultLabel = UILabel()
+        // TODO: Apply string interpolation
+        returnResultLabel.text = "동그라미처럼 보이네요"
+        returnResultLabel.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        returnResultLabel.textColor = .systemGray6
+        return returnResultLabel
+    }()
+    private let similarProportionLabel: UILabel = {
+        let similarProportionLabel = UILabel()
+        // TODO: Apply string interpolation
+        similarProportionLabel.text = "100.0%"
+        similarProportionLabel.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        similarProportionLabel.textColor = .systemGray2
+        return similarProportionLabel
     }()
 
     // MARK:- View life cycle
@@ -43,15 +68,21 @@ extension ViewController {
     private func setConstraints() {
         view.addSubview(blurBackgroundView)
         view.addSubview(drawingView)
+        view.addSubview(buttonStackView)
+        buttonStackView.addArrangedSubview(showingResultButton)
+        buttonStackView.addArrangedSubview(removalButton)
         view.addSubview(labelStackView)
-        labelStackView.addArrangedSubview(showingResultButton)
-        labelStackView.addArrangedSubview(removalButton)
+        labelStackView.addArrangedSubview(returnResultLabel)
+        labelStackView.addArrangedSubview(similarProportionLabel)
 
         blurBackgroundView.translatesAutoresizingMaskIntoConstraints = false
         drawingView.translatesAutoresizingMaskIntoConstraints = false
-        labelStackView.translatesAutoresizingMaskIntoConstraints = false
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
         showingResultButton.translatesAutoresizingMaskIntoConstraints = false
         removalButton.translatesAutoresizingMaskIntoConstraints = false
+        labelStackView.translatesAutoresizingMaskIntoConstraints = false
+        returnResultLabel.translatesAutoresizingMaskIntoConstraints = false
+        similarProportionLabel.translatesAutoresizingMaskIntoConstraints = false
 
         let safeArea = view.safeAreaLayoutGuide
         NSLayoutConstraint.activate([
@@ -66,10 +97,14 @@ extension ViewController {
             drawingView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -24),
             drawingView.heightAnchor.constraint(equalTo: drawingView.widthAnchor),
 
-            labelStackView.topAnchor.constraint(equalTo: drawingView.bottomAnchor, constant: 24),
-            labelStackView.leadingAnchor.constraint(equalTo: drawingView.leadingAnchor),
-            labelStackView.trailingAnchor.constraint(equalTo: drawingView.trailingAnchor),
-            labelStackView.heightAnchor.constraint(equalToConstant: 30)
+            buttonStackView.topAnchor.constraint(equalTo: drawingView.bottomAnchor, constant: 24),
+            buttonStackView.leadingAnchor.constraint(equalTo: drawingView.leadingAnchor),
+            buttonStackView.trailingAnchor.constraint(equalTo: drawingView.trailingAnchor),
+            buttonStackView.heightAnchor.constraint(equalToConstant: 30),
+
+            labelStackView.topAnchor.constraint(equalTo: buttonStackView.bottomAnchor, constant: 32),
+            labelStackView.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
+            labelStackView.heightAnchor.constraint(greaterThanOrEqualToConstant: -100)
         ])
     }
 }
