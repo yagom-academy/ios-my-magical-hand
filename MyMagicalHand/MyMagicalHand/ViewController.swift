0340c22017ec2b21054312fd46ab3336d95b08e1
GREEN <humains29@gmail.com>
2021-04-22 18:14:07 +0900

    [#2] feat: 결과보기 / 지우기 기능 구현

--- MyMagicalHand/MyMagicalHand/ViewController.swift
+++ MyMagicalHand/MyMagicalHand/ViewController.swift
@@ -1,9 +1,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var resultLabel: UILabel!
+    private var shapeName: String = "동그라미"
+    private var probability: String = "100.0"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        resultLabel.isHidden = true
     }
+    
+    @IBAction func clickOnResult(_ sender: UIButton) {
+        resultLabel.text = shapeName + "처럼 보이네요" + "\n" + probability + "%"
+        resultLabel.isHidden = false
+    }
+    
+    @IBAction func clickOnRemove(_ sender: UIButton) {
+        resultLabel.isHidden = true
+    }
+    
+    
+    
 }
 
