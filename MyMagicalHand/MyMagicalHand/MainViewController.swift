ff1654237ee2bef28f2c816bbea6cbfd9c905d0e
lina0322 <llim@student.42seoul.kr>
2021-04-23 19:05:28 +0900

    Feat #2 기본적인 기능들 구현 후 스토리보드와 연결

--- MyMagicalHand/MyMagicalHand/MainViewController.swift
+++ MyMagicalHand/MyMagicalHand/MainViewController.swift
@@ -1,18 +1,43 @@
 //
-//  MyMagicalHand - ViewController.swift
+//  MyMagicalHand - MainViewController.swift
 //  Created by yagom. 
 //  Copyright © yagom. All rights reserved.
 // 
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
+    
+    // MARK: - properties
+    
+    var touchPoint: CGPoint?
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var persentButton: UILabel!
+    @IBOutlet weak var drawingView: UIImageView!
+    
+    // MARK: - life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        clearLabels()
     }
 
-
+    // MARK: - handle buttons
+    
+    @IBAction func touchUpResultButton() {
+        // test message
+        resultLabel.text = "동그라미처럼 보이네요."
+        persentButton.text = "99%"
+    }
+    
+    @IBAction func touchUpClearButton() {
+        clearLabels()
+        drawingView.image = nil
+    }
+    
+    func clearLabels() {
+        resultLabel.text = String.empty
+        persentButton.text = String.empty
+    }
 }
-
