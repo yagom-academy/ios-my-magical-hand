b7f1928311ceb8ace398ea3c6fd1fd06545d0800
GREEN <humains29@gmail.com>
2021-04-25 13:37:31 +0900

    [#3] refactor: 문자열 표시 방식 수정

--- MyMagicalHand/MyMagicalHand/ViewController.swift
+++ MyMagicalHand/MyMagicalHand/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     
     // MARK: - 결과보기 버튼 클릭 액션
     @IBAction func clickOnResult(_ sender: UIButton) {
-        resultLabel.text = shapeName + "처럼 보이네요" + "\n" + probability + "%"
+        resultLabel.text = "\(shapeName)처럼 보이네요 \n \(probability)%"
         resultLabel.isHidden = false
     }
     
