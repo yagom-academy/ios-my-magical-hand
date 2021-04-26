69140ac52462bb32df9e0aa01a6566a2d53a6c1f
Glenn <be9.smile@gmail.com>
2021-04-26 17:44:45 +0900

    [#2] Chore : Canvas View 미완성

---
+++ MyMagicalHand/MyMagicalHand/CanvasView.swift
@@ -0,0 +1,20 @@
+//
+//  CanvasView.swift
+//  MyMagicalHand
+//
+//  Created by iluxsm on 2021/04/23.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
