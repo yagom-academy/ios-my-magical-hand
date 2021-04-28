01c13fa0ec85afb98c67effbb1bb4eee952b0ee0
Joons <elddy0948@gmail.com>
2021-04-28 14:49:07 +0900

    Feat💡 : 결과를 표시해 줄 ResultLabel 구현

--- MyMagicalHand/MyMagicalHand/CustomViews/ResultLabel.swift
+++ MyMagicalHand/MyMagicalHand/CustomViews/ResultLabel.swift
@@ -1,5 +1,26 @@
 import UIKit
 
 class ResultLabel: UILabel {
-
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    init(font: UIFont) {
+        super.init(frame: .zero)
+        configure()
+        self.font = font
+    }
+    
+    private func configure() {
+        textAlignment = .center
+        textColor = .label
+        numberOfLines = 0
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }
