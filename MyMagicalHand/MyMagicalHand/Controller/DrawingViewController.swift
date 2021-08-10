4335cf9cc547c8a82bc7c15012e71b673118abc3
dacodaco <imochi@naver.com>
2021-08-10 16:16:05 +0900

    feat: drawingView layout적용

--- MyMagicalHand/MyMagicalHand/Controller/DrawingViewController.swift
+++ MyMagicalHand/MyMagicalHand/Controller/DrawingViewController.swift
@@ -12,6 +12,16 @@ class DrawingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .gray
+        view.backgroundColor = .darkGray
+        view.layoutMargins = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        self.view.addSubview(drawingView)
+        drawingView.configure()
+
+        NSLayoutConstraint.activate([
+            drawingView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            drawingView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            drawingView.centerYAnchor.constraint(equalTo: view.layoutMarginsGuide.centerYAnchor),
+            drawingView.heightAnchor.constraint(equalTo: view.layoutMarginsGuide.heightAnchor, multiplier: 0.7)
+        ])
     }
 }
--- MyMagicalHand/MyMagicalHand/View/DrawingView.swift
+++ MyMagicalHand/MyMagicalHand/View/DrawingView.swift
@@ -20,6 +20,7 @@ class DrawingView: UIView {
         stackView.alignment = .fill
         stackView.distribution = .fill
         stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
 
@@ -35,38 +36,56 @@ class DrawingView: UIView {
         stackView.axis = .horizontal
         stackView.alignment = .fill
         stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
 
     var resultButton: UIButton = {
         let button = UIButton()
         button.addTarget(DrawingViewController.self, action: #selector(getter: resultButtonAction), for: .touchUpInside)
-        button.titleLabel?.textAlignment = .left
+        button.contentHorizontalAlignment = .leading
+        button.setTitle("결과보기", for: .normal)
+        button.setTitleColor(.systemYellow, for: .normal)
+        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .caption1)
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
     var deleteButton: UIButton = {
         let button = UIButton()
         button.addTarget(DrawingViewController.self, action: #selector(getter: deleteButtonAction), for: .touchUpInside)
-        button.titleLabel?.textAlignment = .right
+        button.contentHorizontalAlignment = .trailing
+        button.setTitle("지우기", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .caption1)
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
     var resultLabel: UILabel = {
         let label = UILabel()
-        label.text = "결과 화면"
-        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.text = "예비 글씨"
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
 
     var percentageToMatch: UILabel = {
         let label = UILabel()
-        label.text = "10%"
+        label.text = "10% 예비 글씨"
         label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = .systemGray5
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
 
     func configure() {
+        self.addSubview(containerView)
+        self.translatesAutoresizingMaskIntoConstraints = false
+
         containerView.addArrangedSubview(canvasView)
         containerView.addArrangedSubview(labelStackView)
         containerView.addArrangedSubview(resultLabel)
@@ -74,6 +93,15 @@ class DrawingView: UIView {
 
         labelStackView.addArrangedSubview(resultButton)
         labelStackView.addArrangedSubview(deleteButton)
+
+        NSLayoutConstraint.activate([
+            containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            containerView.topAnchor.constraint(equalTo: self.topAnchor),
+            containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+
+            canvasView.heightAnchor.constraint(equalTo: canvasView.widthAnchor)
+        ])
     }
 
     func activate(resultButtonAction: @escaping ButtonAction, deleteButtonAction: @escaping ButtonAction) {
